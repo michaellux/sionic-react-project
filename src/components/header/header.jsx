8919323c4bce742069474c6a95d6e2c6bdf002b1
michaellux <michaellux@yandex.ua>
2020-12-24 21:08:55 +0300

    build primary structure

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -3,8 +3,13 @@ import './header.css';
 
 const Header = () => 
   <header>
-    React
+    <h1>React</h1>
+    <img src="" alt="geo"/>
+    <span>Александровск-Са...</span>
+    <input type="search"/>
+    <button><img src="" alt="searchimg"/></button>
+    <button><img src="" alt="basketimg"/></button>
+    <button><img src="" alt="user" /></button>
   </header>
-
 
 export default Header;
