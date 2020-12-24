59b08d0d156d446374b83a00f733fb188da98195
michaellux <michaellux@yandex.ua>
2020-12-24 21:14:57 +0300

    create draft structure of goods page

--- src/components/pages/goods/goods-page.jsx
+++ src/components/pages/goods/goods-page.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import './goods-page.css';
+import CategoryList from '../../category-list';
+import GoodsList from '../../goods-list';
 import Sidebar from '../../sidebar';
 const GoodsPage = () =>
-  <>
+  <main>
     <section>
-      Категории товаров
+      <h2>Категории товаров</h2>
+      <CategoryList/>
+      <GoodsList/>
+      <button>Показать больше товаров</button>
     </section>
     <Sidebar />
-  </>
+  </main>
 
 
 
