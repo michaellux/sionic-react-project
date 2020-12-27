4ab3f459020fee53f18d03e9291e15c3cad521d6
michaellux <michaellux@yandex.ua>
2020-12-27 13:15:27 +0300

    add order form

--- src/components/pages/order/order-page.jsx
+++ src/components/pages/order/order-page.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
+import OrderForm from '../../order';
 import Sidebar from '../../sidebar';
 import './order-page.css';
 
 const OrderPage = () =>
-  <>
+  <main>
     <section>
       Доставка
+      <OrderForm />
     </section>
     <Sidebar />
-  </>
+  </main>
 export default OrderPage;
