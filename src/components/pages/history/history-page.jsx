c86b0c78cbd3e4262235a8c62318944267c5c425
michaellux <michaellux@yandex.ua>
2020-12-27 15:39:58 +0300

    add order history component

--- src/components/pages/history/history-page.jsx
+++ src/components/pages/history/history-page.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
+import OrderhistoryList from '../../orderhistory-list';
 import Sidebar from '../../sidebar';
 import './history-page.css';
 const HistoryPage = () => 
-<>
+<main>
   <section>
     История заказов
+    <OrderhistoryList/>
   </section>
   <Sidebar/>
-</>
+</main>
 
 export default HistoryPage;
