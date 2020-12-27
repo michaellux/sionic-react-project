804459ec7569470962572dbca46fbe74dc880b8f
michaellux <michaellux@yandex.ua>
2020-12-27 10:57:51 +0300

    add child components to cart

---
+++ src/components/cart/cart.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import CartTotal from '../cart-total';
+import CartList from '../cart-list';
+const Cart = () => {
+  return (
+    <div>
+      <CartTotal />
+      <CartList />
+    </div>
+  )
+}
+
+export default Cart;
