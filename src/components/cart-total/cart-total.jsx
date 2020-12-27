df696824656c65468431145e18133f0fc58920b7
michaellux <michaellux@yandex.ua>
2020-12-27 10:56:42 +0300

    create draft structure of summary cart block

---
+++ src/components/cart-total/cart-total.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './cart-total.css';
+const CartTotal = () => {
+  return (
+    <div>
+      <span>Xiaomi</span>
+      <div>
+        <span>Стоимость корзины</span>
+        <strong>1 185 000Р</strong>
+      </div>
+      <button>Оформить</button>
+      <img src="#" alt="cart"/>
+    </div>
+  )
+}
+
+export default CartTotal;
