a7cba7db6b3df03402e5483551901f44160120af
michaellux <michaellux@yandex.ua>
2020-12-27 10:37:06 +0300

    create promo-component for sidebar

---
+++ src/components/sidebar/promo/promo.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Promo = () => {
+  return (
+    <div>
+      <span>Получай товары <strong>бесплатно!</strong></span>
+      <button>Узнать подробнее</button>
+    </div>
+  )
+}
+
+export default Promo;
